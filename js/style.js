2e420b7bc3c31e4f642997e8e921bd3c52c62073
Mahadi Khan <64510042+mksgithub121@users.noreply.github.com>
2020-08-06 19:05:35 +0600

    Update style.js
    content change

--- js/style.js
+++ js/style.js
@@ -1,5 +1,5 @@
 var num = prompt("তুমি আমার থেকে কত বার আই লাভ ইউ শুনতে চাও?");
 
 for (var x = 1; x <= num; x++) {
-    document.write(x + ")" + " I love u Munta <br/>");
+    document.write(x + ")" + " I love u my Jantuus <br/>");
 }
