ad24008abfb8c402209f5a4f496bdee6554ff588
Mahadi Khan <mksbd.net@gmail.com>
2020-08-06 19:01:08 +0600

    promt add

--- js/style.js
+++ js/style.js
@@ -1,3 +1,5 @@
-for (var x = 1; x <= 500; x++) {
+var num = prompt("তুমি আমার থেকে কত বার আই লাভ ইউ শুনতে চাও?");
+
+for (var x = 1; x <= num; x++) {
     document.write(x + ")" + " I love u Munta <br/>");
 }
